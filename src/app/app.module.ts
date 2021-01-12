0460eb1ed283be5e339cb4d22227fc7fea8b6feb
Manish kumar giri <manish.kumar@ults.in>
2021-01-12 19:15:14 +0530

    angular-template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
+import { ProductListsComponent } from './components/shopping-cart/product-lists/product-lists.component';
+import { CartComponent } from './components/shopping-cart/cart/cart.component';
+import { CartItemsComponent } from './components/shopping-cart/cart/cart-items/cart-items.component';
+import { ProductItemComponent } from './components/shopping-cart/product-lists/product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ShoppingCartComponent,
+    FiltersComponent,
+    ProductListsComponent,
+    CartComponent,
+    CartItemsComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
